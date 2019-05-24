f49fbab7f1479b5493b6630939691de9d0605ae7
jinkstudy <sweetyks486@gmail.com>
2019-05-24 18:05:38 +0900

    이벤트 추가

--- pjt2_cafe_order/src/main1/BanaCafe.java
+++ pjt2_cafe_order/src/main1/BanaCafe.java
@@ -26,13 +26,13 @@ public class BanaCafe  extends JFrame {
 		stock = new StockView();
 		try {
 			// 1. ÀÚ¹Ù ³»Àå ·è¾ØÇÊ
-			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+			//UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
 			// 2. quaqua.jar : Quaqua Look and Feel
 
-			   //UIManager.setLookAndFeel("ch.randelshofer.quaqua.QuaquaLookAndFeel");
-			   //JFrame.setDefaultLookAndFeelDecorated(true);
+//			   UIManager.setLookAndFeel("ch.randelshofer.quaqua.QuaquaLookAndFeel");
+//			   JFrame.setDefaultLookAndFeelDecorated(true);
 			// 3. liquidlnf.jar : Liquid Look and Feel
-			    //UIManager.setLookAndFeel("com.birosoft.liquid.LiquidLookAndFeel");
+			    UIManager.setLookAndFeel("com.birosoft.liquid.LiquidLookAndFeel");
 		     // 4. idw-gpl.jar : InfoNode Look and Feel
 			   // UIManager.setLookAndFeel("net.infonode.gui.laf.InfoNodeLookAndFeel");
 			// 5. JTattoo.jar : JTattoo Look and Feel
